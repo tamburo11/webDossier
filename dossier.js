57214eb04ae9dce93b81db85b7b15f7849efc127
tamburo11 <albertobarberis11@gmail.com>
2019-04-16 16:51:40 +0200

    primo commit con i file

---
+++ dossier.js
@@ -0,0 +1,70 @@
+// sticky effect
+let mynavbar = $("#myframe");
+let myjumbo;
+let sticky =mynavbar.offset().top;
+let detectMenuChanges=false;
+
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+    myjumbo = $("#jumbo");
+    
+    if (window.pageYOffset >= sticky-5) {
+        mynavbar.addClass("fixed-top");
+        myjumbo.removeClass("myjumbotron");
+        myjumbo.addClass("myjumbosticky");
+    } else {
+        mynavbar.removeClass("fixed-top");
+        myjumbo.removeClass("myjumbosticky");
+        myjumbo.addClass("myjumbotron");
+    }
+}
+
+let myFirstImage;
+let mySecondImage;
+let myThirdImage;
+let myForthImage;
+
+// inserisco i dati della introduzione
+$(document).ready(function() {
+    $(".container").load("introduction.html");
+    $("#introduction").load("introduction.txt");    
+});
+
+// aggiusto il padding-topo del jambotron a ogni resizing
+// aggiusto la posizione del primo elemento dopo l'immagine a ogni resizing della finestra
+$(window).resize(function(){
+    myFirstImage= $(".firstImage");
+    mySecondImage= $(".secondImage");
+    myThirdImage= $(".thirdImage");
+    myForthImage= $(".fourthImage");
+
+    if(myFirstImage){
+        myFirstImage.parent().next().css("padding-top", (myFirstImage.height()+20)+"px");
+    }
+    if(mySecondImage){
+        mySecondImage.parent().next().css("padding-top", (mySecondImage.height()+20)+"px");
+    }
+    if(myThirdImage){
+        myThirdImage.parent().next().css("padding-top", (myThirdImage.height()+20)+"px");
+    }
+    if(myForthImage){
+        myForthImage.parent().next().css("padding-top", (myForthImage.height()+20)+"px");
+    }
+});
+
+// carico il giusto html a ogni click sulla navbar
+$("ul.navbar-nav li").each(function() {
+   
+    $(this).on("click", function(){
+        $(".container").load($(this).attr("data-page")+".html");
+        $("#myButton").addClass("collapsed");
+        $("#myButton").attr("aria-expanded","false");
+        $("#navbarCollapse").removeClass("show");
+    });
+});
+
+// carico il cv.html quando si clicca sul nome
+$(".navbar-brand").on("click", function(){
+        $(".container").load("cv.html");
+});
---
+++ readme.txt
@@ -0,0 +1,1 @@
+- node server.js per aprirei ls server dal terminale
